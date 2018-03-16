98acc196aa1a7c5e870c4af522c125bb07aaed21
Navid <yaa110@gmail.com>
2018-03-16 15:16:37 +0330

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! # RAKE implementation for Rust
+//!
+//! The library provides a Rust implementation of [Rapid Automatic Keyword Extraction (RAKE)][1] algorithm.
+//!
+//! [1]: http://onlinelibrary.wiley.com/doi/10.1002/9780470689646.ch1/summary
+
+#![doc(html_root_url = "https://docs.rs/rake/0.1")]
+#![deny(missing_docs)]
