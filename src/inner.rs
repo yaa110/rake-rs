0448eeb660f54478efd3025eceacac9e0bbe1065
Navid <yaa110@gmail.com>
2018-10-21 12:59:42 +0330

    Fix number checker to support multilingual

--- src/inner.rs
+++ src/inner.rs
@@ -1,9 +1,15 @@
+use regex::Regex;
+
+lazy_static! {
+    static ref NUM_RE: Regex = Regex::new(r"-?\p{N}+[./٫,']?\p{N}*").unwrap();
+}
+
 pub(crate) trait NumberChecker {
     fn is_number(&self) -> bool;
 }
 
 impl<'a> NumberChecker for &'a str {
     fn is_number(&self) -> bool {
-        self.parse::<f64>().is_ok()
+        NUM_RE.is_match(self)
     }
 }
